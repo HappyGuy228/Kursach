a03f3c636dc5d2b0ba6b8f8c50254477d7195207
ammarunov <90957477+ammarunov@users.noreply.github.com>
2023-04-24 16:38:07 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ from keyboards import kb_client_settings, kb_client_menu, kb_client_review
 from aiogram.types import ReplyKeyboardRemove
 from inline import keyboard
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+from data_base import sqlite_db
 
 # import client
 
@@ -16,7 +20,7 @@ dp = Dispatcher(bot, storage=storage)
 
 async def on_startup(_):
     print("Я запустился!")
-
+    sqlite_db.sql_start()
 
 HELP_MESSAGE = """Данный бот является магазином по продаже обуви
 В разделе 'Каталог' Вы можете найти список всех товаров, отсортированных по категориям
@@ -105,6 +109,50 @@ async def process_category(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await callback_query.message.reply(f'Это {category}!')
 
+class FSMAdmin(StatesGroup):
+    name = State()
+    phone = State()
+    address = State()
+
+# Работа с загрузкой товаров. Подключение к БД. Потом привяжем к кнопкам, настроим доступы и тд :Р
+@dp.message_handler(commands='Загрузить', state=None)
+async def cm_start(message: types.Message):
+    await FSMAdmin.name.set()
+    await message.reply('Введите ваше ФИО.')
+
+@dp.message_handler(state="*", commands='отмена')
+@dp.message_handler(Text(equals='отмена', ignore_case=True), state="*")
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('OK')
+
+@dp.message_handler(state=FSMAdmin.name)
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+    await FSMAdmin.next()
+    await message.reply("Введите ваш номер")
+
+
+@dp.message_handler(state=FSMAdmin.phone)
+async def load_number(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['phone'] = int(message.text)
+    await FSMAdmin.next()
+    await message.reply("Укажите ваш адрес")
+
+
+@dp.message_handler(state=FSMAdmin.address)
+async def load_address(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['address'] = message.text
+    # async with state.proxy() as data:
+    #     await message.reply(str(data))
+    await sqlite_db.sql_add_command(state)
+    await state.finish()
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
